45b16a92be349688829acb1d9fa5d822b7fbe629
Xuri Li <dlshle@hotmail.com>
2019-06-04 22:01:33 -0500

    finished v0.00000000001

--- new_parser.js
+++ new_parser.js
@@ -37,7 +37,7 @@ class Stack {
 	
 	nodesAsArray() {
 		let temp = this.head;
-		result = [];
+		let result = [];
 		while (temp != null) {
 			result.push(temp.val);
 			temp = temp.next;
@@ -93,8 +93,9 @@ function makeRegex (mPack) {
 		let s = generateRegStr(mPack[e].s);
 		e = generateRegStr(e);
 		closeTags += ("(" + e + ")" + "|");
-		openTags += ("(" + s + ")" + "|");
-		regex += "(" + e + ")|" + "(" + s + ")|";
+		if (s != "")
+			openTags += ("(" + s + ")" + "|");
+		regex += "(" + e + ")|" + (s != ""?"(" + s + ")|":"");
 	}
 	//to remove the last |s
 	closeTags = closeTags.substring(0, closeTags.length - 1);
@@ -103,6 +104,7 @@ function makeRegex (mPack) {
 	return {o:new RegExp(openTags, "g"), c:new RegExp(closeTags, "g"), r:new RegExp(regex, "g")};
 }
 
+//append escape signs 
 function generateRegStr (s) {
 	let result = "";
 	let special = "()[]{}\"\'\\/";
@@ -117,7 +119,7 @@ function generateRegStr (s) {
 
 class Parser {
 	//TODO:
-	//pre_reg is the str, post_reg is the str, both is for the real regex
+	//Testing convertTags function
 	constructor (original, mPack) {
 		this.original = original;
 		this.mPack = mPack;
@@ -126,17 +128,16 @@ class Parser {
 		this.openTags = regexs.o;
 		this.closeTags = regexs.c;
 		this.regex = regexs.r;
-		//tokenize
-		let tokens = this.tokenize(this.original);
 		//stack algo
-		this.tree = this.buildParsingTree(tokens);
-		//try to expand the tree	
-		this.expandTree(this.tree);
+		this.tree = this.buildParsingTree(this.tokenize(this.original));
+		//try to expand the tree	for debugging only
+		//this.expandTree(this.tree);
 	}
 
 	//tokenize function classifies tokens in the text by checking if a token is a tag or a pure text
 	//matches = str.match(regex); matches = ["xx", "x*", "x.x"]...
 	//matches would be an array of every open and close tags
+	//make an array of objects of form {type: (text|open|close), content: (text|tag)}
 	tokenize () {
 		let tokens = new Stack();
 		let last = 0;
@@ -155,6 +156,7 @@ class Parser {
 		return tokens;
 	}
 
+	//build the parsing tree of pNodes
 	buildParsingTree (tokens) {
 		let root = new pNode("root", null);
 		while (!tokens.isEmpty()) {
@@ -181,6 +183,9 @@ class Parser {
 		let recursive = this.mPack[token.content].r;
 		let target = this.mPack[token.content].s;
 		let root = new pNode("tag", {o:null, c:token.content});
+		//if it's a unary tag, just return w/ open tag = null
+		if (target === "")
+			return root;
 		while (!tokens.isEmpty()) {
 			let curr = tokens.pop();
 			if (curr.type === "open" && curr.content === target) {
@@ -202,32 +207,36 @@ class Parser {
 				if (recursive) {
 					//shouldn't have an unclosed open tag here
 					console.log("ERROR: unclosed tag spotted!\nThis tag will be regarded as a text.");
-					root.addChild(new pNode("text", token.content));
+					root.addChild(new pNode("text", curr.content));
 				} else {
-					root.addChild(new pNode("text", token.content));
+					root.addChild(new pNode("text", curr.content));
 				}
 			}
 		}
 		if (!root.content.o) {
 			//if this tag is unmatched, convert it to the text node
 			root.type = "text";
-			root.content = root.content.c;
+			console.log("unmatched closing tag found!");
+			console.log(root);
 		}
 		return root;
 	}
 
+	//check if a tag is an open tag
 	isOpenTag (token) {
 		let result = this.openTags.exec(token) == null?false:true;
 		this.openTags.lastIndex = 0;
 		return result;
 	}
 
+	//check if a tag is a close tag
 	isCloseTag (token) {
 		let result = this.closeTags.exec(token).length == null?false:true;
 		this.closeTags.lastIndex = 0;
 		return result;
 	}
 
+	//simply print out the tree in order
 	expandTree (root) {
 		console.log(root);
 		let layer = root.children.nodesAsArray();
@@ -236,10 +245,30 @@ class Parser {
 		}
 	}
 
+	//recursively convert texts
+	convertTags (mPack, root) {
+		//text could contain children as it could be an unclosed tag
+		if (root.children.isEmpty())
+			return root.content;
+		let convertedText = "";
+		let nodes = root.children.nodesAsArray();
+		for (let i = 0; i < nodes.length; i++) {
+			convertedText += this.convertTags(mPack, nodes[i]);
+		}
+		if (root.type === "root")
+			return convertedText;
+		else if (root.type === "text")
+			return convertedText + root.content.c;
+		return mPack[root.content.c].f(root.content.o, root.content.c, convertedText);
+	}
+
+	parse () {
+		return this.convertTags(this.mPack, this.tree);
+	}
 }
 
 function simpleReplace(o, c, t) {
-	return o.substring(1, o.lastIndexOf("]")) + t + c.substring(1, c.lastIndexOf("]"));
+	return "<" + o.substring(1, o.lastIndexOf("]")) + ">" + t + "<" + c.substring(1, c.lastIndexOf("]")) + ">"
 }
 
 function bold(open, close, text) {
@@ -258,6 +287,10 @@ function quote(open, close, text) {
 	return simpleReplace(open, close, text);
 }
 
+function hr(open, close, text) {
+	return "<hr>";
+}
+
 //a markdown language usually contains normal texts and tags. A tag maps a function from one markdown language to another.
 //a mapping pack describes how tags in a markdown language behaves and defines the mapping function of each tag. 
 //usually, a parser is constructed with the original markdown text and a mapping pack along with mapping functions, and then it generates one regex that contains all the open tags and close tags.
@@ -265,9 +298,11 @@ function quote(open, close, text) {
 //
 //in the mapping pack, a open tag has three elements, s(start with), r(recursive folding(recursively convert inner nodes)), f(converting or mapping fucntion)
 //f should take 3 params open, close, content
+//if it's a unary tag, s = ""
 var eb2h = {
 	"[/b]": {s: "[b]", r: true, f: bold},
 	"[/i]": {s: "[i]", r: true, f: italic}, 
 	"[/u]": {s: "[u]", r: true, f: underline}, 
-	"[/q]": {s: "[q]", r: true, f:quote}
+	"[/q]": {s: "[q]", r: true, f: quote},
+	"[hr]": {s: "", r: false, f: hr}
 };
